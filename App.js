6ad81ee8839216c3f997ad0ae50e9c5d6a3b8923
Eduardo <hobold.edu@hotmail.com>
2020-11-17 22:46:55 -0300

    Implementando estrutura do Mapa

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Login from './src/pages/Login/login'
 import Cadastro from './src/pages/Cadastro/cadastro'
+import Mapa from './src/pages/Mapa/mapa'
 
 const App = () => {
 	return (
-		<Login />
+		<Mapa />
 	);
 };
 
---
+++ src/pages/Mapa/mapa.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import MapView from 'react-native-maps'
+
+function Cadastro() {
+	return (
+		<View style={styles.container}>
+
+			<MapView
+				style={styles.map}
+				loadingEnabled={true}
+				region={{
+					latitude: 37.78825,
+					longitude: -122.4324,
+					latitudeDelta: 0.0922,
+					longitudeDelta: 0.0421,
+				}}
+			>
+
+			</MapView>
+
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+
+	container: {
+		flex: 1,
+		justifyContent: 'flex-end',
+		alignItems: 'flex-end',
+	},
+
+	map: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+	},
+
+});
+
+export default Cadastro;
